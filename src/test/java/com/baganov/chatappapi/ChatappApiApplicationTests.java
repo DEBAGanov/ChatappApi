d0e505730b423ba9c1351696e8ab65d1870714b7
Vladimir Baganov <baganov.v@gmail.com>
2025-02-27 20:04:45 +0300

    int

---
+++ src/test/java/com/baganov/chatappapi/ChatappApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baganov.chatappapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatappApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
