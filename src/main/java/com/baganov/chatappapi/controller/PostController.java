bf125789007ca820b007ba58c6d96f0bc53cf5a7
Vladimir Baganov <baganov.v@gmail.com>
2025-03-23 18:20:12 +0300

    добавил новый файл миграции с добавлением данных V2. Реализовал пагинанацию данных.

--- src/main/java/com/baganov/chatappapi/controller/PostController.java
+++ src/main/java/com/baganov/chatappapi/controller/PostController.java
@@ -3,12 +3,13 @@ package com.baganov.chatappapi.controller;
 import com.baganov.chatappapi.model.Post;
 import com.baganov.chatappapi.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/posts")
@@ -18,8 +19,8 @@ public class PostController {
     private PostRepository postRepository;
 
     @GetMapping
-    public ResponseEntity<List<Post>> getAllPosts() {
-        List<Post> posts = postRepository.findAll();
+    public ResponseEntity<Page<Post>> getAllPosts(@PageableDefault(size = 100) Pageable pageable) {
+        Page<Post> posts = postRepository.findAll(pageable);
         return ResponseEntity.ok(posts);
     }
 }
